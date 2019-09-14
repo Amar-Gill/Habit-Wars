b6cc7b254466f6caa0c1acc5ab06270182651ca0
Amar Gill <amar.s.gill@outlook.com>
2019-09-14 21:04:18 +0800

    able to update db with diceroll form client

--- web_app/blueprints/rounds/views.py
+++ web_app/blueprints/rounds/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, flash, abort, url_for
+from flask import Blueprint, render_template, request, redirect, flash, abort, url_for, jsonify
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 from models.user import User
 from models.log_habit import LogHabit
+from models.game import Game
+from models.round import Round
 from models.habit import Habit
 import peewee as pw
 from config import Config
@@ -19,13 +21,25 @@ def show():
     # hardcode game_id for now
     game_id = 1
 
-    num_dice = []
+    #get game model object
+    game = Game.get_by_id(game_id)
+
+    #empty array for number of dice
+    dice_array = []
 
     #get round id
     round_id = 1 #hardcode for dev purposes
 
     #get current player id - hardcoded for now
     current_player = User.get_by_id(1)
+
+    #determine if current_user is p1 or p2
+    game_player_1_id = game.player_1_id
+    # if current_user.id == game_player_1_id:
+    if current_player.id == game_player_1_id:
+        player_variable = 1
+    else:
+        player_variable = 2
 
 
     #querydb get habits with user_id==current user AND game_id==game_id
@@ -46,10 +60,23 @@ def show():
         #compare length of list to frequency number
         if len(approved_logs) >= habit.frequency:
             #append to dice list
-            num_dice += [1]
+            dice_array += [1]
 
-    return render_template('rounds/show.html', num_dice=num_dice)
+    return render_template('rounds/show.html', dice_array=dice_array, player_variable=player_variable, game_id=game_id)
 
-@rounds_blueprint.route('/roll')
-def roll_dice():
-    pass
+@rounds_blueprint.route('/game=<game_id>/player=<player>/roll', methods=['POST'])
+def roll_dice(game_id, player):
+    roll_value = int(request.form.get('roll_value'))
+    roll_index = int(request.form.get('roll_index'))
+
+    # get round instance - do we need game_id????
+    round = Round.get_by_id(1)
+
+    if player == '1':
+        round.player_1_rolls[roll_index] = roll_value
+        round.save()
+    else:
+        round.player_2_rolls[roll_index] = roll_value
+        round.save()
+
+    return 'LEEROY FUCKIN JENKINZ'
