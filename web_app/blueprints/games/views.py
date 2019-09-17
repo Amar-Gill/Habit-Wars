67e9b4556a0a89d03e366b5e571575a43e934ad0
Chee Haw <chimharry@hotmail.com>
2019-09-17 18:14:31 +0800

    user profile page and new game page done

--- web_app/blueprints/games/views.py
+++ web_app/blueprints/games/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, flash, abort, u
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 from models.game import Game
+from models.user import User
+from models.habit import Habit
 import peewee as pw
 from config import Config
 
@@ -14,19 +16,28 @@ games_blueprint = Blueprint('games',
 def new_game_page():
     return render_template('games/new_game_page.html')
 
-@games_blueprint.route('create_new_games')
+@games_blueprint.route('create_new_games', methods=['POST', 'GET'])
 def create_new_game():
-    player_1 = User.get_or_none(User.id == current_user.id)
-    player_2_email = request.form.get('p2_email')
-    player_2 = User.get_or_none(User.email == player_2_email)
+    
+    player_1 = User.get_or_none(User.username == "user3")
+    player_2 = User.get_or_none(User.username == request.form["p2_name"])
     new_game = Game(player_1 = player_1, player_2 = player_2)
     new_game.save()
 
-    p1_habit_a = Habit(game=new_game, user=player_1, name=request.form.get('habit_1_name'), frequency=request.form.get('habit_1_frequency'))
-    p1_habit_b = Habit(game=new_game, user=player_1, name=request.form.get('habit_2_name'), frequency=request.form.get('habit_2_frequency'))
-    p1_habit_c = Habit(game=new_game, user=player_1, name=request.form.get('habit_3_name'), frequency=request.form.get('habit_3_frequency'))
+    habit_a = Habit(game = new_game, user = player_1, 
+    name = request.form["habit_1_name"], 
+    frequency=request.form["habit_1_value"])
 
-    p1_habit_a.save()
-    p1_habit_b.save()
-    p1_habit_c.save()
-    return render_template('games/new_game_page.html')
+    habit_b = Habit(game = new_game, user = player_1, 
+    name = request.form["habit_2_name"], 
+    frequency=request.form["habit_2_value"])
+
+    habit_c = Habit(game = new_game, user = player_1, 
+    name = request.form["habit_3_name"], 
+    frequency=request.form["habit_3_value"])
+
+    habit_a.save()
+    habit_b.save()
+    habit_c.save()
+
+    return redirect(url_for('games.new_game_page'))
