32e1eeb2c4268b3ce261bc96922e43fd0c9db869
Gabriella <gabriella@meggy.es>
2019-09-18 11:49:34 +0800

    p2 habitbars tested

--- web_app/blueprints/games/views.py
+++ web_app/blueprints/games/views.py
@@ -37,18 +37,28 @@ def show(username, game_id):
         logged_habits = len(approved_logs)
         percentage = logged_habits / habit.frequency * 100
         progress.append(percentage)
-        rounded_progress = [round(freq, 0) for freq in progress]
-        print(rounded_progress)
+
+    rounded_progress = [round(freq, 0) for freq in progress]
 
     #Player 2
 
-    print(game.player_2_id)
     player_2 = User.get_or_none(User.id == game.player_2_id)
     player_2_username = player_2.username
 
     player2_habits = Habit.select().where((Habit.game_id == game_id) & (Habit.user_id == player_2.id))
     p2_habit_length = len(player2_habits)
-    print(p2_habit_length)
+
+    #Progress bars
+
+    p2_progress = []
+
+    for habit in player2_habits:
+        approved_logs = LogHabit.select().where((LogHabit.sender_id == player_2.id) & (LogHabit.habit_id == habit.id) & (LogHabit.approved == True) & (LogHabit.game_round_id == 2))        
+        logged_habits = len(approved_logs)
+        percentage = logged_habits / habit.frequency * 100
+        p2_progress.append(percentage)
+    
+    rounded_p2_progress = [round(freq, 0) for freq in p2_progress]
 
     return render_template('games/show.html', 
                             username = user.username, 
@@ -57,4 +67,7 @@ def show(username, game_id):
                             rounded_progress = rounded_progress, 
                             player_2_username = player_2_username,
                             player2_habits = player2_habits,
-                            p2_habit_length = p2_habit_length)
+                            p2_habit_length = p2_habit_length,
+                            rounded_p2_progress = rounded_p2_progress)
+
+#Dont forget to change game round id
